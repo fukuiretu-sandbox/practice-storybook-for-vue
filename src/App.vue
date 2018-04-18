8df235054453aaad87e2bd6e3323a9596f2a8502
fukuiretu <fukuiretu@gmail.com>
2018-04-18 15:46:54 +0900

    Add modal

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,49 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld/>
+
+    <button id="show-modal" @click="showModal = true">Show Modal</button>
+
+    <!-- use the modal component, pass in the prop -->
+    <confirm-modal v-if="showModal" @close="showModal = false">
+      <h2 slot="header">hoge</h2>
+      <!--
+        you can use custom content here to overwrite
+        default content
+      -->
+      <h3 slot="header">custom header</h3>
+
+      <div slot="footer">
+        <span>custom footer</span>
+        <button class="modal-default-button" @click="hello('hoge')">
+          OK
+        </button>
+      </div>
+    </confirm-modal>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import Modal from './components/modal/Modal'
+import ConfirmModal from './components/modal/ConfirmModal'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Modal,
+    ConfirmModal
+  },
+  data() {
+    return {
+      showModal: false
+    }
+  },
+  methods: {
+    hello(name) {
+      alert(`Hello, ${name}`)
+    }
   }
 }
 </script>
---
+++ src/components/modal/ConfirmModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <modal>
+    <div slot="footer">
+      <button class="modal-default-button" @click="ok">
+        OK
+      </button>
+      <button class="modal-default-button" @click="$emit('close')">
+        close
+      </button>
+    </div>
+  </modal>
+</template>
+
+<script>
+import Modal from "./Modal";
+
+export default {
+  components: {
+    Modal
+  },
+  methods: {
+    ok() {
+      alert('OK');
+    }
+  }
+}
+</script>
